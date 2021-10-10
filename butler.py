fd3fd1a0716cae409c8632b347c0fac8b5ec43a2
Michael Sanford <michaelsanford@users.noreply.github.com>
2021-10-10 00:17:17 -0400

    Pylint improvements

--- butler.py
+++ butler.py
@@ -1,6 +1,8 @@
+# pylint: disable=C0321, C0114, W0702, C0103, C0301, R1710
 from os import getenv
-import serial
 from time import sleep
+import sys
+import serial
 import sanity
 
 BAUDRATE = int(getenv('BAUDRATE', '57600'))
@@ -12,22 +14,22 @@ DTMF_DURATION = getenv('DTMF_DURATION', '255')
 if BAUDRATE not in sanity.BAUDRATES:
     print(
         f"FATAL: 'BAUDRATE' of {BAUDRATE} provided is invalid, must be one of {sanity.BAUDRATES}.")
-    exit(1)
+    sys.exit(1)
 
 if DIAL not in sanity.DIALABLES:
     print(
         f"FATAL: Provided 'DIAL' of {DIAL} is invalid, must be one of {sanity.DIALABLES}.")
-    exit(1)
+    sys.exit(1)
 
 if not 1 <= int(DTMF_DURATION) <= 255:
     print(
         f"FATAL: Provided 'DTMF_DURATION' of {DTMF_DURATION} is invalid, must be between 1 and 255.")
-    exit(1)
+    sys.exit(1)
 
 if not 1 <= TIMEOUT <= sanity.TIMOUT_MAX:
     print(
-        f"FATAL: Provided 'TIMEOUT' of {TIMEOUT} is invalid, must be between 1 and {sys.maxsize}.")
-    exit(1)
+        f"FATAL: Provided 'TIMEOUT' of {TIMEOUT} is invalid, must be between 1 and {sanity.TIMOUT_MAX}.")
+    sys.exit(1)
 
 
 # Open the modem
@@ -35,14 +37,16 @@ modem = serial.Serial(port='/dev/ttyACM0', baudrate=BAUDRATE,
                       timeout=TIMEOUT, write_timeout=TIMEOUT)
 
 
-def init_modem():
+def configure_modem():
+    """Configure the modem or terminate the program"""
+
     try:
         modem.flushInput()
         modem.flushOutput()
 
         if not AT():
             print("FATAL: Unable to reach the modem.")
-            exit(2)
+            sys.exit(2)
 
         if not AT("Z"):
             print("ERROR: Unable to reset the modem.")
@@ -52,29 +56,35 @@ def init_modem():
 
         if not AT("X0"):
             print("FATAL: Unable to disable dial done detection.")
-            exit(3)
+            sys.exit(3)
 
         if not AT(f"S11={DTMF_DURATION}"):
             print("ERROR: Unable to set S11 register.")
 
     except:
         print("FATAL: Unable to initialize the modem.")
-        exit(1)
+        sys.exit(1)
 
 
 def AT(command=""):
+    """Sends an AT command to the modem
+
+    Keyword arguments:
+    command -- A Hayes-compatible AT command (default "").
+    """
+
     try:
         modem.flushInput()
         modem.flushOutput()
 
         modem.write(f"AT{command}\r".encode())
 
-        response = modem.readline() + modem.readline()
-        response = response.rstrip()
+        r = modem.readline() + modem.readline()
+        r = r.rstrip()
 
-        if response is not "".encode():
-            print(response.decode())
-            return "OK" in response.decode()
+        if r is not "".encode():
+            print(r.decode())
+            return "OK" in r.decode()
 
     except:
         print(Exception)
@@ -87,7 +97,7 @@ def AT(command=""):
 #         h.write(f"OK {time.ctime()}" if ok else "FAILED")
 
 
-init_modem()
+configure_modem()
 
 
 print("LISTENING...")
--- sanity.py
+++ sanity.py
@@ -1,3 +1,6 @@
+"""
+Sanity checks for environment-based configuration provided by the user.
+"""
 import sys
 
 BAUDRATES = (50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800,
